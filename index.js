19d6b404f2b89c6797b33b8dcf88266dc725700d
Gustavo <gustavonaldoni1@gmail.com>
2025-10-14 18:59:41 -0300

    versão 1.4

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.use(methodOverride('_method'))
 
 const urlMongo = "mongodb+srv://admin:admin@cluster0.huwt4el.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const nomeBanco = 'sistemaBioenergy'
-const nomeAdmin = 'admin'
 const collectionName = 'usuarios'
+const collectionServico = 'servicos'
+const collectionManu = 'manutenções'
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/index.html')
@@ -36,7 +37,7 @@ app.post('/registro', async (req,res)=>{
     try {
         await cliente.connect()
         const banco = cliente.db(nomeBanco)
-        const colecaoUsuarios = banco.collection('usuarios')
+        const colecaoUsuarios = banco.collection(collectionName)
 
         const usuarioExistente = await colecaoUsuarios.findOne({usuario: req.body.usuario})
         if(usuarioExistente){
@@ -46,8 +47,8 @@ app.post('/registro', async (req,res)=>{
             await colecaoUsuarios.insertOne({
                 usuario: req.body.usuario,
                 senha: senhaCriptografada,
-                tipo: 'comum' // ou 'admin' se quiser criar manualmente
-                })
+                tipo: 'comum'
+            })
             res.redirect('/login')
         }
     }catch(erro){
@@ -66,7 +67,7 @@ app.post('/login', async (req,res)=>{
     try {
         await cliente.connect()
         const banco = cliente.db(nomeBanco)
-        const colecaoUsuarios = banco.collection('usuarios')
+        const colecaoUsuarios = banco.collection(collectionName)
 
         const usuario = await colecaoUsuarios.findOne({usuario: req.body.usuario})
 
@@ -74,7 +75,6 @@ app.post('/login', async (req,res)=>{
             req.session.usuario = req.body.usuario;
             req.session.tipo = usuario.tipo;
 
-            // Redireciona de acordo com o tipo
             if (usuario.tipo === 'admin') {
                 res.redirect('/admin')
             } else {
@@ -89,7 +89,6 @@ app.post('/login', async (req,res)=>{
         cliente.close()
     }
 })
-
 
 function protegerRota(req,res,proximo){
     if(req.session.usuario){
@@ -115,34 +114,64 @@ app.get('/admin', protegerAdmin, (req, res) => {
   res.sendFile(__dirname + '/views/admin/index.html');
 });
 
+app.get('/api/usuarios', protegerAdmin, async (req, res) => {
+  const client = new MongoClient(urlMongo)
+  try {
+    await client.connect()
+    const db = client.db(nomeBanco)
+    const collection = db.collection(collectionName)
+
+    const usuarios = await collection.find({}, { projection: { _id: 1, usuario: 1 } }).toArray()
+    res.json(usuarios)
+  } catch (err) {
+    console.error('Erro ao buscar usuários: ', err)
+    res.status(500).send('Erro ao buscar usuários')
+  } finally {
+    client.close()
+  }
+})
+
 app.get('/crud_usuarios', protegerAdmin, (req,res) =>{
-    res.sendFile(__dirname + '/views/admin/views/usuarios.html')
+    res.sendFile(__dirname + '/views/admin/html/usuarios.html')
+})
+
+app.get('/crud_servicos', protegerAdmin, (req,res) =>{
+    res.sendFile(__dirname + '/views/admin/html/serviços.html')
+})
+
+app.get('/crud_manutencoes', protegerAdmin, (req,res) =>{
+    res.sendFile(__dirname + '/views/admin/html/manutencoes.html')
 })
 
 app.get('/crud_usuarios_cadastro', protegerAdmin, (req,res)=>{
-    res.sendFile(__dirname + '/views/admin/views/usuarios_cadastro.html')
+    res.sendFile(__dirname + '/views/admin/html/usuarios/cadastrar.html')
+})
+
+app.get('/crud_servicos_cadastro', protegerAdmin, (req,res)=>{
+    res.sendFile(__dirname + '/views/admin/html/servicos/cadastrar.html')
+})
+
+app.get('/crud_manutencoes_cadastro', protegerAdmin, (req,res)=>{
+    res.sendFile(__dirname + '/views/admin/html/manutencao/cadastrar.html')
 })
 
 app.post('/crud_usuarios_cadastro', protegerAdmin, async (req,res)=>{
    const novoUsuario = req.body
-
    const client = new MongoClient(urlMongo)
 
    try {
        await client.connect()
+       const db = client.db(nomeBanco)
+       const collection = db.collection(collectionName)
 
-        const db = client.db(nomeAdmin)
-        const collection = db.collection(collectionName)
-
-        const senhaCriptografada = await bcrypt.hash(novoUsuario.senha, 10)
-        await collection.insertOne({
-        usuario: novoUsuario.usuario,
-        senha: senhaCriptografada,
-        tipo: novoUsuario.tipo || 'comum'
-        })
-        console.log(`Usuário cadastrado com sucesso. ID: ${result.insertedId}`)
-
-        res.redirect('/admin')
+       const senhaCriptografada = await bcrypt.hash(novoUsuario.senha, 10)
+       const result = await collection.insertOne({
+          usuario: novoUsuario.usuario,
+          senha: senhaCriptografada,
+          tipo: novoUsuario.tipo || 'comum'
+       })
+       console.log(`Usuário cadastrado com sucesso. ID: ${result.insertedId}`)
+       res.redirect('/admin')
         
    }catch(err){
     console.error('Erro ao cadastrar o usuário: ', err)
@@ -152,23 +181,72 @@ app.post('/crud_usuarios_cadastro', protegerAdmin, async (req,res)=>{
    }
 })
 
+app.post('/crud_servicos_cadastro', protegerAdmin, async (req,res)=>{
+   const novoServico = req.body
+   const client = new MongoClient(urlMongo)
+
+   try {
+       await client.connect()
+       const db = client.db(nomeBanco)
+       const collection = db.collection(collectionServico)
+
+       const result = await collection.insertOne(novoServico)
+       console.log(`Serviço cadastrado com sucesso. ID: ${result.insertedId}`)
+       res.redirect('/admin')
+        
+   }catch(err){
+    console.error('Erro ao cadastrar o serviço: ', err)
+    res.status(500).send('Erro ao cadastrar o serviço. Por favor tente mais tarde ')
+   }finally{
+    client.close()
+   }
+})
+
+app.post('/crud_manutencoes_cadastro', protegerAdmin, async (req,res)=>{
+   const novaManu = req.body
+   const client = new MongoClient(urlMongo)
+
+   try {
+       await client.connect()
+       const db = client.db(nomeBanco)
+       const collection = db.collection(collectionManu)
+
+       const result = await collection.insertOne(novaManu)
+       console.log(`Manutenção cadastrada com sucesso. ID: ${result.insertedId}`)
+       res.redirect('/admin')
+        
+   }catch(err){
+    console.error('Erro ao cadastrar a manutenção: ', err)
+    res.status(500).send('Erro ao cadastrar a manutenção. Por favor tente mais tarde ')
+   }finally{
+    client.close()
+   }
+})
+
+
 app.get('/crud_usuarios_atualizar', protegerAdmin, async(req,res)=>{
-    res.sendFile(__dirname + '/views/admin/views/usuarios/atualizar.html')
+    res.sendFile(__dirname + '/views/admin/html/usuarios/atualizar.html')
+})
+
+app.get('/crud_servicos_atualizar', protegerAdmin, async(req,res)=>{
+    res.sendFile(__dirname + '/views/admin/html/servicos/atualizar.html')
+})
+
+app.get('/crud_manutencoes_atualizar', protegerAdmin, async(req,res)=>{
+    res.sendFile(__dirname + '/views/admin/html/manutencao/atualizar.html')
 })
 
 app.post('/crud_usuarios_atualizar', protegerAdmin, async(req,res)=>{
-    const { id, usuario, senha } = req.body
-
+    const { id, usuario, senha, tipo } = req.body
     const client = new MongoClient(urlMongo)
 
     try {
         await client.connect()
-
-        const db = client.db(nomeAdmin)
+        const db = client.db(nomeBanco)
         const collection = db.collection(collectionName)
         const senhaCriptografada = await bcrypt.hash(senha, 10)
         const result = await collection.updateOne({ _id: new ObjectId(id)},{
-            $set: {usuario, senha: senhaCriptografada}
+            $set: {usuario, senha: senhaCriptografada, tipo}
         })
         if(result.modifiedCount > 0){
             console.log(`Usuário com o ID: ${id} atualizado com sucesso`)
@@ -184,19 +262,68 @@ app.post('/crud_usuarios_atualizar', protegerAdmin, async(req,res)=>{
     }
 })
 
+
+app.post('/crud_servicos_atualizar', protegerAdmin, async(req,res)=>{
+    const { id, usuario, tipo, custoHora, custoTotal, dias, disponivel } = req.body
+    const client = new MongoClient(urlMongo)
+
+    try {
+        await client.connect()
+        const db = client.db(nomeBanco)
+        const collection = db.collection(collectionServico)
+        const result = await collection.updateOne({ _id: new ObjectId(id)},{
+            $set: {usuario,  tipo, custoHora, custoTotal, dias, disponivel}
+        })
+        if(result.modifiedCount > 0){
+            console.log(`Serviço com o ID: ${id} atualizado com sucesso`)
+            res.redirect('/admin')
+        }else{
+            res.status(404).send('Serviço não encontrado')
+        }
+    }catch(err){
+        console.error('Erro ao atualizar o serviço: ', err)
+        res.status(500).send('Erro ao atualizar o Serviço. Por favor tente novamente mais tarde.')
+    }finally{
+        client.close()
+    }
+})
+
+app.post('/crud_manutencoes_atualizar', protegerAdmin, async(req,res)=>{
+    const { id, usuario, tipo, custoHora, custoTotal, dias, disponivel } = req.body
+    const client = new MongoClient(urlMongo)
+
+    try {
+        await client.connect()
+        const db = client.db(nomeBanco)
+        const collection = db.collection(collectionManu)
+        const result = await collection.updateOne({ _id: new ObjectId(id)},{
+            $set: {usuario,  tipo, custoHora, custoTotal, dias, disponivel}
+        })
+        if(result.modifiedCount > 0){
+            console.log(`Manutenção com o ID: ${id} atualizado com sucesso`)
+            res.redirect('/admin')
+        }else{
+            res.status(404).send('Manutenção não encontrada')
+        }
+    }catch(err){
+        console.error('Erro ao atualizar a manutenção: ', err)
+        res.status(500).send('Erro ao atualizar a manutenção. Por favor tente novamente mais tarde.')
+    }finally{
+        client.close()
+    }
+})
 app.get('/usuario/:id', protegerAdmin, async (req,res)=>{
     const { id } = req.params
-
     const cliente = new MongoClient(urlMongo)
 
     try{
         await cliente.connect()
-        const db = cliente.db(nomeAdmin)
+        const db = cliente.db(nomeBanco)
         const collection = db.collection(collectionName)
 
         const usuario = await collection.findOne({_id: new ObjectId(id)})
 
-        if(!livro){
+        if(!usuario){
             return res.status(404).send('Usuário não encontrado')
         }
         res.json(usuario)
@@ -208,15 +335,58 @@ app.get('/usuario/:id', protegerAdmin, async (req,res)=>{
     }
 })
 
+app.get('/servico/:id', protegerAdmin, async (req,res)=>{
+    const { id } = req.params
+    const cliente = new MongoClient(urlMongo)
+
+    try{
+        await cliente.connect()
+        const db = cliente.db(nomeBanco)
+        const collection = db.collection(collectionServico)
+
+        const serviço = await collection.findOne({_id: new ObjectId(id)})
+
+        if(!serviço){
+            return res.status(404).send('Serviço não encontrado')
+        }
+        res.json(serviço)
+    }catch(err){
+        console.error('Erro ao buscar o serviço: ', err)
+        res.status(500).send('Erro ao buscar o serviço. Por favor tente novamente mais tarde')
+    }finally{
+        cliente.close()
+    }
+})
+app.get('/manutencao/:id', protegerAdmin, async (req,res)=>{
+    const { id } = req.params
+    const cliente = new MongoClient(urlMongo)
+
+    try{
+        await cliente.connect()
+        const db = cliente.db(nomeBanco)
+        const collection = db.collection(collectionManu)
+
+        const manu = await collection.findOne({_id: new ObjectId(id)})
+
+        if(!manu){
+            return res.status(404).send('Manutenção não encontrado')
+        }
+        res.json(manu)
+    }catch(err){
+        console.error('Erro ao buscar a manutenção: ', err)
+        res.status(500).send('Erro ao buscar a manutenção. Por favor tente novamente mais tarde')
+    }finally{
+        cliente.close()
+    }
+})
+
 app.post('/crud_usuarios_deletar', protegerAdmin, async(req,res)=>{
     const {id} = req.body
-
-    const client = new MongoClient(url)
+    const client = new MongoClient(urlMongo)
 
     try{
         await client.connect()
-
-        const db = client.db(nomeAdmin)
+        const db = client.db(nomeBanco)
         const collection = db.collection(collectionName)
 
         const result = await collection.deleteOne({_id: new ObjectId(id)})
@@ -235,15 +405,64 @@ app.post('/crud_usuarios_deletar', protegerAdmin, async(req,res)=>{
     }
 })
 
+app.post('/crud_servicos_deletar', protegerAdmin, async(req,res)=>{
+    const {id} = req.body
+    const client = new MongoClient(urlMongo)
+
+    try{
+        await client.connect()
+        const db = client.db(nomeBanco)
+        const collection = db.collection(collectionServico)
+
+        const result = await collection.deleteOne({_id: new ObjectId(id)})
+
+        if(result.deletedCount > 0){
+            console.log(`Serviço com ID: ${id} deletado com sucesso`)
+            res.redirect('/admin')
+        }else{
+            res.status(404).send('Serviço não encontrado')
+        }
+    }catch(err){
+        console.log('Erro ao deletar o serviço:', err)
+        res.status(500).send('Erro ao deletar o serviço. Por favor tente novamente mais tarde')
+    }finally{
+        client.close()
+    }
+})
+
+app.post('/crud_manutencoes_deletar', protegerAdmin, async(req,res)=>{
+    const {id} = req.body
+    const client = new MongoClient(urlMongo)
+
+    try{
+        await client.connect()
+        const db = client.db(nomeBanco)
+        const collection = db.collection(collectionManu)
+
+        const result = await collection.deleteOne({_id: new ObjectId(id)})
+
+        if(result.deletedCount > 0){
+            console.log(`Manutenção com ID: ${id} deletado com sucesso`)
+            res.redirect('/admin')
+        }else{
+            res.status(404).send('Manutenção não encontrada')
+        }
+    }catch(err){
+        console.log('Erro ao deletar a manutenção:', err)
+        res.status(500).send('Erro ao deletar a manutenção. Por favor tente novamente mais tarde')
+    }finally{
+        client.close()
+    }
+})
 app.get('/crud_usuarios_usuarios', protegerAdmin, async(req,res)=>{
     const cliente = new MongoClient(urlMongo)
 
     try{
         await cliente.connect()
-        const db = cliente.db(nomeAdmin)
+        const db = cliente.db(nomeBanco)
         const collection = db.collection(collectionName)
 
-        const usuarios = await collection.find({}, {projection: {_id:1, usuario:1, senha:1 }}).toArray()
+        const usuarios = await collection.find({}, {projection: {_id:1, usuario:1, senha:1, tipo:1 }}).toArray()
         res.json(usuarios)
     }catch(err){
         console.error('Erro ao buscar usuários: ', err)
@@ -252,6 +471,43 @@ app.get('/crud_usuarios_usuarios', protegerAdmin, async(req,res)=>{
         cliente.close()
     }
 })
+
+app.get('/crud_servicos_servicos', protegerAdmin, async(req,res)=>{
+    const cliente = new MongoClient(urlMongo)
+
+    try{
+        await cliente.connect()
+        const db = cliente.db(nomeBanco)
+        const collection = db.collection(collectionServico)
+
+        const usuarios = await collection.find({}, {projection: {_id:1, usuario:1, tipo:1, custoHora:1, custoTotal:1, dias:1, disponivel:1 }}).toArray()
+        res.json(usuarios)
+    }catch(err){
+        console.error('Erro ao buscar servicos: ', err)
+        res.status(500).send('Erro ao buscar servicos. Por favor tente novamente mais tarde.')
+    }finally{
+        cliente.close()
+    }
+})
+
+app.get('/crud_manutencoes_manutencoes', protegerAdmin, async(req,res)=>{
+    const cliente = new MongoClient(urlMongo)
+
+    try{
+        await cliente.connect()
+        const db = cliente.db(nomeBanco)
+        const collection = db.collection(collectionManu)
+
+        const usuarios = await collection.find({}, {projection: {_id:1, usuario:1, tipo:1, custoHora:1, custoTotal:1, dias:1, disponivel:1 }}).toArray()
+        res.json(usuarios)
+    }catch(err){
+        console.error('Erro ao buscar manutenções: ', err)
+        res.status(500).send('Erro ao buscar manutenções. Por favor tente novamente mais tarde.')
+    }finally{
+        cliente.close()
+    }
+})
+
 app.get('/erro', (req,res)=>{
     res.sendFile(__dirname + '/views/erro.html')
 })
@@ -267,4 +523,3 @@ app.get('/sair', (req,res)=>{
 app.listen(porta, ()=>{
     console.log(`Servidor rodando na porta ${porta}`)
 })
-
